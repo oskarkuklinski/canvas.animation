a3eb93b56b40c6097383e7edea2047d2639c820f
oskarkuklinski <oskar.kuklinski@wp.pl>
2017-06-13 22:10:36 +0100

    Update anim.js

--- anim.js
+++ anim.js
@@ -1,11 +1,12 @@
+// CANVAS AND CONTEXT
+
 var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-//var grd = c.createLinearGradient(0, 0, canvas.width, 0);
-//grd.addColorStop(0, 'black');
-//grd.addColorStop(1, 'rgb(0, 0, 255)');
+
+// ARRAY WITH COLORS
 
 var colorArray = [
     "#D8CAA8",
@@ -15,22 +16,40 @@ var colorArray = [
     "#363942"
     ];
 
+// OBJECTS FOR MOUSE MOVE AND MOUSE DOWN
+
 var mouse = {
     x: undefined,
     y: undefined
 }
+var mouseclick = {
+    x: undefined,
+    y: undefined
+}
 
-var maxRadius = 50;
+// EVENT LISTENERS FOR MOUSE DOWN, MOUSE MOVE AND RESIZE
 
-window.addEventListener('mousemove', function(event){
+window.addEventListener('mousemove', function(event) {
     mouse.x = event.x;
     mouse.y = event.y;
 });
 
-window.addEventListener('resize', function(event){
+window.addEventListener('resize', function(event) {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 });
+
+window.addEventListener('click', function(event) {
+    mouseclick.x = event.x;
+    mouseclick.y = event.y;  
+});
+
+// VALUES OF COUNTER AND MAX-RADIUS
+
+var counter = 0;
+var maxRadius = 50;
+
+// CONSTRUCTOR FOR CIRCLES
 
 function Circle(x, y, dx, dy, radius, color) {
     this.x = x;
@@ -41,14 +60,16 @@ function Circle(x, y, dx, dy, radius, color) {
     this.minRadius = radius;
     this.color = color;
     
+    // METHOD TO DRAW A CIRCLE ON CANVAS
+    
     this.draw = function() {
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
-//        c.fillStyle = grd;
-//        c.fill();
         c.fillStyle = color;
         c.fill();
-    }
+    };
+    
+    // UPDATING CANVAS, CREATING ANIMATION
     
     this.update = function() {
         if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
@@ -62,18 +83,34 @@ function Circle(x, y, dx, dy, radius, color) {
         this.x += this.dx;
         this.y += this.dy
         
-        // interactivity
-        if ((mouse.x - this.x) < 50 && (mouse.x - this.x) > -50 && (mouse.y - this.y) < 50 && (mouse.y - this.y) > -50) {
-            if (this.radius < maxRadius) {
-                this.radius += 1;
-            }
-        } else if (this.radius > this.minRadius){
-            this.radius -= 1;   
+        // INTERACTIVITY
+        // ON MOUSE CLICK
+        
+        if ((mouseclick.x - this.x) < this.radius && (mouseclick.x - this.x) > -this.radius && (mouseclick.y - this.y) < this.radius && (mouseclick.y - this.y) > -this.radius) {
+            this.radius = 0;
+            counter += 1;
+            console.log(counter);
         }
         
+        //ON MOUSE MOVE
+        
+        if (this.radius > 0) {
+            if ((mouse.x - this.x) < 50 && (mouse.x - this.x) > -50 && (mouse.y - this.y) < 50 && (mouse.y - this.y) > -50) {
+                if (this.radius < maxRadius) {
+                    this.radius += 1; 
+                }
+            } else if (this.radius > this.minRadius) {
+                this.radius -= 1;   
+            }
+        }
+        
+        // PLAYING DRAW METHOD INSIDE UPDATE METHOD
+        
         this.draw();
-    }   
-}
+    };   
+};
+
+// CREATING CIRCLES WITH AN ARRAY AND ADDING VALUES
 
 var circleArray = [];
 
@@ -87,13 +124,21 @@ for (var i=0; i<300; i++) {
     circleArray.push(new Circle(x, y, dx, dy, radius, color));
 }
 
+function scoreBoard() {
+    c.font = "40px Roboto Mono";
+    c.fillText("Score: " + counter, 200, 200);
+}
+
+// ANIMATE FUNCTION
+
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth, innerHeight);
     
     for (var i=0; i<circleArray.length; i++) {
-        circleArray[i].update();   
+        circleArray[i].update(); 
     }
+    scoreBoard();
 }  
 
 animate();
